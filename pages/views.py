f105e9bb1618a4244a8db2226547db4ca1ee6fbb
Jesus Bernal <jlbernal2@buffs.wtamu.edu>
2026-09-04 18:45:50 -0500

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    goals = [
+        {"name": "Maintain an A or B in all my classes", "completed": True},
+        {"name": "Collect my dream Choso figure", "completed": False},
+        {"name": "Get a new car by the end of the year", "completed": False},
+        {"name": "Get a new monitor for my computer", "completed": True},
+        {"name": "Become a better cooker", "completed": False},
+    ]
+    return render(request, "home.html", {"goals": goals})
+
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
